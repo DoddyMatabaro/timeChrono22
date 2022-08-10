1d0d57f79d1939486631ec569c4f469dac6ca552
Doddy MATABARO <matabarododdy@gmail.com>
2022-08-10 10:43:52 +0200

    header section tags

---
+++ src/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <header>
+        <div>Logo</div>
+        <nav>
+            Menu
+        </nav>
+    </header>
+    
+  )
+}
+
+export default Header
