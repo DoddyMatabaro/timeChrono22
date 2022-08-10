dad67b8f98bbca1419191e65912fe52c8f365f01
Doddy MATABARO <matabarododdy@gmail.com>
2022-08-10 10:45:43 +0200

    clock component to display clock container

---
+++ src/Clock/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ClockItems from '../ClockItems'
+import './clock.css';
+function Clock() {
+  return (
+    <section id="clock">
+        <ClockItems />
+    </section>
+  )
+}
+
+export default Clock
