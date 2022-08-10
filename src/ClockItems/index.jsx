f5b94f00d9fd2a3294ff168968cfcf472c6f56bd
Doddy MATABARO <matabarododdy@gmail.com>
2022-08-10 16:12:00 +0200

    reajust styles fro same elements

--- src/ClockItems/index.jsx
+++ src/ClockItems/index.jsx
@@ -16,12 +16,6 @@ const ExpiredNotice = () =>{
 const ShowCounter = ({days, hours, minutes, seconds})=>{
   return (
     <div className="show-counter">
-      <a
-        href="https://tapasadhikary.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="countdown-link"
-      >
         <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
         <p>:</p>
         <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
@@ -29,7 +23,7 @@ const ShowCounter = ({days, hours, minutes, seconds})=>{
         <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
         <p>:</p>
         <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-      </a>
+
     </div>
   );
 };
